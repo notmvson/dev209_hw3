32416c93ac5e844407f25cc92cf852a3cb1fcc85
notmvson <mason5kong@yahoo.com>
2024-01-30 11:57:45 -0800

    finished js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
     document.getElementById("newUserInfo").disabled = true;
     document.getElementById('existingUserInfo').disabled = true;
 
@@ -11,5 +12,45 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById("existingUserInfo").disabled = false;
         document.getElementById("userType").disabled = true;
     })
+
+    document.getElementById("newLoginButton").addEventListener("click", function() {
+        newUserSubmitEvent();
+    })
     
+    function newUserSubmitEvent() {
+        let userName = document.getElementById("newUserName").value;
+        let password = document.getElementById("newPassword").value;
+        let city = document.getElementById("city").value;
+        let state = document.getElementById("state").value;
+
+        if (userName !== "" && password !== "" && city !== "" && state.length === 2) {
+            localStorage.setItem("userName", userName);
+            localStorage.setItem("password", password);
+            localStorage.setItem("city", city);
+            localStorage.setItem("state", state);
+            alert("Welcome " + userName);
+        }
+        else {
+            alert("Please fill in all the fields");
+        }
+    }
+
+    document.getElementById("existingLoginButton").addEventListener("click", function() {
+        existingUserSubmitEvent();
+    })
+
+    function existingUserSubmitEvent() {
+        let userName = document.getElementById("existingUserName").value;
+        let password = document.getElementById("existingPassword").value;
+
+        console.log(localStorage.getItem("userName"));
+        console.log(localStorage.getItem("password"));
+
+        if (userName === localStorage.getItem("userName") && password === localStorage.getItem("password")) {
+            alert("Welcome back " + userName);
+        }
+        else {
+            alert("Invalid login");
+        }
+    }
 })
