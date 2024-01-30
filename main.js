03087a2e16ba1954ba68030f2caa94ebacebdb75
notmvson <mason5kong@yahoo.com>
2024-01-30 10:59:54 -0800

    added forms, fieldsets, and button event listeners

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("newUserInfo").disabled = true;
+    document.getElementById('existingUserInfo').disabled = true;
+
+    document.getElementById("newUser").addEventListener("click", function() {
+        document.getElementById("newUserInfo").disabled = false;
+        document.getElementById("userType").disabled = true;
+    })
+
+    document.getElementById("existingUser").addEventListener("click", function() {
+        document.getElementById("existingUserInfo").disabled = false;
+        document.getElementById("userType").disabled = true;
+    })
     
 })
